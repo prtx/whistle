6d374b5e9faa36d95d9d0e672f62b06a625d2d89
prtx <prtx.pratikshrestha@gmail.com>
2017-12-20 20:36:46 +0545

    Amplitudes in graph

--- music.py
+++ music.py
@@ -1,5 +1,6 @@
 import scipy.io.wavfile as wav
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Music():
@@ -11,6 +12,25 @@ class Music():
         if type(self.amplitude[0]) == np.ndarray:
             self.amplitude = (self.amplitude[:,0] + self.amplitude[:,1])/2
    
+
     def __str__(self):
         info = (self.file_name, self.frame_rate, str(self.amplitude))
         return "Filename : %s\nFrame Rate : %d\nAmplitudes : %s" % info
+    
+
+    def plot(self):
+        plt.clf()
+        plt.plot(np.arange(0, len(self.amplitude))/self.frame_rate, self.amplitude)
+
+        plt.title(self.file_name)
+        plt.xlabel('Time')
+        plt.ylabel('Amplitude')
+        
+        plt.show()
+
+
+
+if __name__ == '__main__':
+    music = Music('G.wav')
+    print(music)
+    music.plot()
